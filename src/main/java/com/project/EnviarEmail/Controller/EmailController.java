ec4efd051af32899d63eaefe8d6f57369806e80c
Mario_junior23 <mariojunior3251@gmail.com>
2020-10-03 16:27:08 -0300

    Criando diretriz para enviar email

---
+++ src/main/java/com/project/EnviarEmail/Controller/EmailController.java
@@ -0,0 +1,33 @@
+package com.project.EnviarEmail.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmailController {
+    
+	@Autowired private JavaMailSender mailSender;
+	
+	@RequestMapping(path = "/enviar-email", method = RequestMethod.GET)
+	 public String sendEmail() {
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setText("Ola sou uma aplicação Springboot");
+		message.setTo("mariojunior3251@gmail.com");
+		message.setFrom("mariojunior3251@gmail.com");
+		
+		try { 
+			mailSender.send(message);
+			return "Email enviado com sucesso";
+		}catch (Exception e) {
+			e.printStackTrace();
+			return "erro ao enviar email.";
+		}
+		
+	}
+	
+}
+
