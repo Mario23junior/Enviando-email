cbbda59f9a32af7722e99eb25fca1fdfad7f5da7
Mario_junior23 <mariojunior3251@gmail.com>
2020-10-03 05:08:46 -0300

    Iniciando projeto de enviar email

---
+++ src/test/java/com/project/EnviarEmail/EnviarEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.EnviarEmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnviarEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
